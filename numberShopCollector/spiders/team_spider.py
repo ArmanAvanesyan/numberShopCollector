a439ae08790383113ec82d5b0bb027bd44cb5d30
ArmanAvanesyan <91835739+ArmanAvanesyan@users.noreply.github.com>
2024-10-07 06:28:29 +0400

    Update team_spider.py

--- numberShopCollector/spiders/team_spider.py
+++ numberShopCollector/spiders/team_spider.py
@@ -1,19 +1,21 @@
 import scrapy
-from numberShopCollector.numberShopCollector.loaders import TeamItemLoader
-from numberShopCollector.numberShopCollector.selectors import TeamSelectors
+from numberShopCollector.numberShopCollector.loaders import UcomItemLoader
+from numberShopCollector.numberShopCollector.selectors import UcomSelectors
 
 
-class TeamSpider(scrapy.Spider):
-    name = "team"
-    allowed_domains = ["www.telecomarmenia.am"]
-    start_urls = ["https://www.telecomarmenia.am/eshop/en/numbers/"]
+class UcomSpider(scrapy.Spider):
+    name = 'ucom'
+    allowed_domains = ["shop.ucom.am"]
+    start_urls = ["https://shop.ucom.am/en/colored-numbers.html"]
 
     def parse(self, response):
-        for product in response.css(TeamSelectors.PRODUCT):
-            loader = TeamItemLoader(selector=product)
-            loader.add_css('mobile_number', TeamSelectors.MOBILE_NUMBER)
+        for product in response.css(UcomSelectors.PRODUCT):
+            loader = UcomItemLoader(selector=product)
+            loader.add_css('mobile_number', UcomSelectors.MOBILE_NUMBER)
+            loader.add_css('status', UcomSelectors.STATUS)
             yield loader.load_item()
 
-        next_page = response.css(TeamSelectors.NEXT_PAGE).get()
+        next_page = response.css(UcomSelectors.NEXT_PAGE).get()
         if next_page:
-            yield response.follow(next_page, callback=self.parse)
+            next_page_url = response.urljoin(next_page)
+            yield scrapy.Request(next_page_url, callback=self.parse)
